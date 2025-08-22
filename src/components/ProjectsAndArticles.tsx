aef389ef28ca3a1b8efeda869631ac4d2f9e32f2
piresdesign <brunopires134@gmail.com>
2025-08-21 22:04:14 -0300

    novo artigo - escolher

--- src/components/ProjectsAndArticles.tsx
+++ src/components/ProjectsAndArticles.tsx
@@ -6,6 +6,17 @@ import { ChevronLeft, ChevronRight, ExternalLink, Calendar, Tag } from "lucide-r
 const portfolioItems = [
   {
     id: 1,
+    title: "Por que escolher é tão cansativo?",
+    description:
+      "O que a Lei de Hick e o Paradoxo da Escolha nos ensinam sobre decisões rápidas e interfaces mais eficientes.",
+    type: "Artigo",
+    date: "2025-08-21",
+    category: "UX Collective BR",
+    image: "/images/escolher.png",
+    link: "https://medium.com/ux-user-experience-design-em-portugues/por-que-escolher-é-tão-cansativo-be1fb3e6f0b1",
+  },
+  {
+    id: 2,
     title: "UX, interfaces amigáveis e apostas: O que isso tem em comum?",
     description:
       "Como princípios do design e gamificação manipulam a percepção de ganho e influenciam no comportamento de usuários em plataformas de apostas.",
@@ -16,7 +27,7 @@ const portfolioItems = [
     link: "https://brasil.uxdesign.cc/ux-interfaces-amigaveis-e-apostas-o-que-isso-tem-em-comum-fb716e00856c",
   },
   {
-    id: 2,
+    id: 3,
     title: "Tempo subjetivo e UX: Quando cada segundo conta",
     description: "Esperar nunca é só questão de tempo. Relaciona-se diretamente como nos sentimos enquanto esperamos. O design tem um papel fundamental nisso.",
     type: "Artigo",
@@ -26,7 +37,7 @@ const portfolioItems = [
     link: "https://medium.com/ux-user-experience-design-em-portugues/tempo-subjetivo-e-ux-quando-cada-segundo-conta-e9bfdc0fcdf0",
   },
   {
-    id: 3,
+    id: 4,
     title: "Por que o cérebro adora padrões? A ciência por trás da previsibilidade no design digital",
     description: "Como a mente humana percebe padrões e como isso pode transformar a experiência.",
     type: "Artigo",
@@ -36,7 +47,7 @@ const portfolioItems = [
     link: "https://medium.com/ux-user-experience-design-em-portugues/por-que-o-cérebro-adora-padrões-a-ciência-por-trás-da-previsibilidade-no-design-digital-626b108700cb",
   },
   {
-    id: 4,
+    id: 5,
     title: "Espaço em branco e UX: O invisível que estrutura a experiência",
     description: "Reflexões, estudos, impactos, aspectos psicológicos e cognitivos que relacionam a arquitetura visual e o espaço em branco.",
     type: "Artigo",
@@ -46,7 +57,7 @@ const portfolioItems = [
     link: "https://medium.com/ux-user-experience-design-em-portugues/espaco-em-branco-e-ux-o-invisivel-que-estrutura-a-experiencia-3425257739de",
   },
   {
-    id: 5,
+    id: 6,
     title: "Negociando com negócio: Como falar com stakeholders",
     description: "Como designers podem ir além da criação de interfaces e se tornar verdadeiros negociadores nas equipes de produto.",
     type: "Artigo",
@@ -56,7 +67,7 @@ const portfolioItems = [
     link: "https://medium.com/ux-user-experience-design-em-portugues/negociando-com-negócio-como-falar-com-stakeholders-b92048643c78",
   },
   {
-    id: 6,
+    id: 7,
     title: "No palco do produto: O espetáculo entre design e tecnologia",
     description: "O que há em comum entre designers e desenvolvedores? Por que se relacionam no fluxo de um produto digital? Qual a importância desse relacionamento?",
     type: "Artigo",
@@ -66,7 +77,7 @@ const portfolioItems = [
     link: "https://medium.com/ux-user-experience-design-em-portugues/no-palco-do-produto-o-espetáculo-entre-design-e-tecnologia-fafc7098c599",
   },
   {
-    id: 7,
+    id: 8,
     title: "Melhor Gestão - Mobile App Design",
     description: "Atuei como UX/UI Designer na concepção do aplicativo MelhorGestão. O app está atualmente em desenvolvimento e deve ser disponibilizado nas lojas de aplicativo em breve.",
     type: "Projeto",
@@ -76,7 +87,7 @@ const portfolioItems = [
     link: "https://www.behance.net/gallery/214773165/Melhor-Gestao-Mobile-App-Design",
   },
   {
-    id: 8,
+    id: 9,
     title: "UX/UI Design - ShopNice",
     description: "Aplicativo com conceitos de gamificação que busca aumentar a visitação e consumo em centros comerciais. Desenvolvido durante o curso de UI Design da Awari.",
     type: "Projeto",
@@ -86,7 +97,7 @@ const portfolioItems = [
     link: "https://www.behance.net/gallery/135321137/UXUI-Design-ShopNice",
   },
   {
-    id: 9,
+    id: 10,
     title: "Estudo de caso UX/UI Design - Organize",
     description: "Ajudando pessoas a organizar as suas tarefas com mais de uma ferramenta dentro de um mesmo aplicativo.",
     type: "Projeto",
@@ -96,7 +107,7 @@ const portfolioItems = [
     link: "https://brunopires134.medium.com/estudo-de-caso-organize-b9977fec7f3b",
   },
   {
-    id: 10,
+    id: 11,
     title: "Case - Ready",
     description: "Ajudando leitores a encontrarem recomendações, registrar leituras, organizar suas bibliotecas e conhecer pessoas que compartilhem do mesmo interesse. Meu primeiro estudo de caso, desenvolvido em 2021.",
     type: "Projeto",
